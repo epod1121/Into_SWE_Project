4e6079c1840436507fac5879e34003676ad6aae9
Ethan Podhorez <p.ethanpod@gmail.com>
2026-04-01 14:56:30 -0400

    Starting us off

---
+++ database.sql
@@ -0,0 +1,12 @@
+-- this should be fairly straight forward for you
+-- you can honestly wait until js is done connecting
+-- to the html file and accessing the data before you need to
+-- code anything so you know the names of what to put down
+-- i would reccomend that you at least get the main format out
+-- of the way so you have the shell of what you need to code 
+
+-- i got the very first line out of the way but add some comments
+-- or even some more solid usable code so that when js is done
+-- you can just throw the names of everything in and call it a day
+
+CREATE DATABASE UserData;
---
+++ functionality.js
@@ -0,0 +1,8 @@
+// there will probably be some tricky things you need to do
+// for this project and i am more than willing to help if you need it
+// let me know if you need any help with the specifics of connecting with
+// the html file but most of actual mathy-coding is straight forward :)
+// javascript is like if java and python had a baby so a lot of the code 
+// will look familiar just typed differently
+
+// you got this and dont hesitate to reach out in the groupme!
