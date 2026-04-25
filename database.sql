12b6c0b2e438f58ba26512e1ee2629353461188c
Ethan Podhorez <p.ethanpod@gmail.com>
2026-04-25 16:53:13 -0400

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,16 +1,3 @@
--- this should be fairly straight forward for you
--- you can honestly wait until js is done connecting
--- to the html file and accessing the data before you need to
--- code anything so you know the names of what to put down
--- i would reccomend that you at least get the main format out
--- of the way so you have the shell of what you need to code 
-
--- i got the very first line out of the way but add some comments
--- or even some more solid usable code so that when js is done
--- you can just throw the names of everything in and call it a day
-
--- General Planner Database
-
 DROP DATABASE IF EXISTS general_planner;
 CREATE DATABASE general_planner;
 USE general_planner;
@@ -57,6 +44,20 @@ CREATE TABLE events (
 );
 
 -- =========================
+-- CLASSES TABLE
+-- =========================
+
+CREATE TABLE classes (
+    class_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    class_name VARCHAR(100) NOT NULL,
+    class_day ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'),
+    class_time TIME,
+    location VARCHAR(100),
+    CONSTRAINT fk_classes_users FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+-- =========================
 -- SAMPLE DATA
 -- =========================
 
@@ -80,4 +81,3 @@ VALUES
     'Work',
     FALSE
 );
-
